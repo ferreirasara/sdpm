4ba4029e0ae6f5154ec2077a7e22aec845590db4
ferreirasara <ferreirasara1501@hotmail.com>
2021-08-12 15:24:56 -0300

    feat: algorithm list

---
+++ frontend/src/utils/algorithmList.ts
@@ -0,0 +1,26 @@
+export const algorithmList = [
+  {
+    name: "optimalAlgorithm",
+    label: "Algoritmo Ótimo"
+  },
+  {
+    name: "fifoAlgorithm",
+    label: "Algoritmo FIFO"
+  },
+  {
+    name: "secondChanceAlgorithm",
+    label: "Algoritmo Segunda Chance"
+  },
+  {
+    name: "lruAlgorithm",
+    label: "Algoritmo LRU"
+  },
+  {
+    name: "nruAlgorithm",
+    label: "Algoritmo NRU"
+  },
+  {
+    name: "wsClockAlgorithm",
+    label: "Algoritmo WS-Clock"
+  },
+]
