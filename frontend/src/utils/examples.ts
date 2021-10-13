b96e7498e81ce26435075e3305b54114dc0a1f3e
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2021-10-12 21:04:42 -0300

    fix: add field actionsQueue in examples

--- frontend/src/utils/examples.ts
+++ frontend/src/utils/examples.ts
@@ -6,6 +6,7 @@ const example1 = {
   numberOfPages: 6,
   pages: ['A1', 'A2', 'A3', 'B1', 'B2', 'B3'],
   pagesQueue: 'A1|A2|A3|A2|A3|B2|B3|A1|A2|A3|A2|A1|A3',
+  actionsQueue: 'E|E|L|E|E|L|E|L|L|L|L|E|E',
   memoryInitalState: '0|0|0',
   tau: 1,
 }
@@ -16,6 +17,7 @@ const example2 = {
   numberOfPages: 6,
   pages: ['A1', 'A2', 'A3', 'B1', 'B2', 'B3'],
   pagesQueue: 'A2|A3|B2|B3|A1|A2|A3|B2|B1|B3|B2|A3',
+  actionsQueue: 'L|E|E|L|L|E|L|L|E|E|E|L',
   memoryInitalState: 'A1|A2|A3',
   tau: 1,
 }
