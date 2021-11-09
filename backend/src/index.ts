4bcc7e2c871a5f4f0528c06e6b0d7387d6eb33bf
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2021-11-09 18:06:36 -0300

    fix: request limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,11 +17,8 @@ dao.initializeDB().then().catch(error => console.log(error));
 const app: express.Application = express();
 
 app.use(cors());
-// support application/json type post data
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
-//support application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
 app.post("/simulation", [
   validateSimulationRequiredParamsMiddleware,
