a5304cbc8d2d5c2083a596782a2ed486c8fab69e
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2021-11-08 23:55:13 -0300

    fix: request limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,8 @@ const app: express.Application = express();
 
 app.use(cors());
 // support application/json type post data
-app.use(bodyParser.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 //support application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: false }));
 
