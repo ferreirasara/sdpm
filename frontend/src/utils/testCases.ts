f71270096fb5120a00849f182a16598d5f4cd24e
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2021-11-11 21:39:16 -0300

    fix: fix clockInterruption in test cases

--- frontend/src/utils/testCases.ts
+++ frontend/src/utils/testCases.ts
@@ -20,7 +20,7 @@ const testCase2 = {
   pagesQueue: "G|D|G|C|G|G|C|C|G|I|C|G|A|A|I|J|C|E|A|C|I|A|E|H|G|I|E|J|B|A|F|H|B|D|E|D|E|I|I|A|A|I|A|B|J|A|E|E|J|B",
   actionsQueue: "E|L|L|E|L|E|L|L|L|L|E|E|L|L|E|E|E|L|L|E|E|E|L|E|E|E|L|L|E|E|L|E|L|E|E|E|L|E|E|L|L|L|E|E|E|E|E|E|E|L",
   memoryInitalState: "G|D|0|0|A",
-  clockInterruption: 3,
+  clockInterruption: 5,
   tau: 1,
 }
 
@@ -32,7 +32,7 @@ const testCase3 = {
   pagesQueue: "J|G|G|D|G|G|J|H|F|D|H|F|F|G|C|A|I|H|D|E|A|E|E|G|B|I|B|J|I|F|A|J|E|J|G|E|G|F|D|I|I|H|A|E|G|H|G|H|E|A",
   actionsQueue: "L|E|E|L|L|L|E|E|E|E|L|L|E|L|E|L|E|L|L|E|L|E|L|E|E|L|L|L|L|L|L|L|L|L|L|L|E|E|E|E|E|L|E|E|L|L|E|E|E|L",
   memoryInitalState: "F|E|I|D|A",
-  clockInterruption: 3,
+  clockInterruption: 10,
   tau: 4,
 }
 
