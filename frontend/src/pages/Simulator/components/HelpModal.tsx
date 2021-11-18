2a315824d005b2003197e01f8da21ebda79e5f62
Sara Cristina Ferreira <ferreirasara1501@hotmail.com>
2021-11-18 13:32:37 -0300

    fix: fix misspelling

--- frontend/src/pages/Simulator/components/HelpModal.tsx
+++ frontend/src/pages/Simulator/components/HelpModal.tsx
@@ -21,20 +21,20 @@ export default function HelpModal(props: HelpModalProps) {
         <Space direction="vertical">
           <Typography.Text strong mark>Primeiro, preencha os dados para a simulação</Typography.Text>
           <span>Os campos "Fila de páginas", "Fila de ações", "Estado inicial da memória", "Interrupção do relógio" e "τ (tau)" podem ser gerados automaticamente.</span>
-          <span>O campo "Interrupção do relógio" é utilizado somente quando os algoritmos "NRU", "Segunda Chance" ou "WS-Clock" são selecionados</span>
+          <span>O campo "Interrupção do relógio" é utilizado somente quando os algoritmos "NRU", "Segunda Chance" ou "WS-Clock" são selecionados.</span>
           <span>O Campo "τ (tau)" é utilizado somente quando o algoritmo "WS-Clock" é selecionado.</span>
         </Space>
       </Timeline.Item>
       <Timeline.Item color="#ffe58f">
         <Space direction="vertical">
-          <Typography.Text strong mark>Botões extras.</Typography.Text>
-          <span>Gerar dados aleatórios: Caso queira, você pode gerar dados aleatórios para a simulação</span>
+          <Typography.Text strong mark>Botões extras</Typography.Text>
+          <span>Gerar dados aleatórios: Caso queira, você pode gerar dados aleatórios para a simulação.</span>
           <span>Limpar: limpa todos os dados do formulário.</span>
         </Space>
       </Timeline.Item>
       <Timeline.Item color="#ffe58f">
         <Space direction="vertical">
-          <Typography.Text strong mark>Após clicar em "Simular", aguarde alguns instantes.</Typography.Text>
+          <Typography.Text strong mark>Após clicar em "Simular", aguarde alguns instantes</Typography.Text>
           <span>Dependendo da quantidade de dados, a simulação pode demorar um pouco.</span>
           <span>Devido ao site de hospedagem utilizado, a API de simulação "dorme" após 30 minutos sem uso. Nesse caso, é comum que o processo de simulação demore um pouco mais, pois precisa esperar a API iniciar novamente.</span>
         </Space>
